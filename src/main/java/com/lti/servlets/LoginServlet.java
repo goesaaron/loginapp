4f8fe5e60769e2dce02ad000adb12e304078d882
goesaaron <goesaaron@gmail.com>
2020-11-19 17:24:34 +0530

    addingrepo

---
+++ src/main/java/com/lti/servlets/LoginServlet.java
@@ -0,0 +1,52 @@
+package com.lti.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		String uname=request.getParameter("name");
+		String password=request.getParameter("password");
+		
+		RequestDispatcher rd=null;
+		if(uname.equals("aaron")&&password.equals("123")) {
+			rd=request.getRequestDispatcher("Success.html");
+			rd.forward(request, response);
+		}else {
+			rd=request.getRequestDispatcher("Failure.html");
+			rd.forward(request, response);
+		}
+		
+	}
+
+}
